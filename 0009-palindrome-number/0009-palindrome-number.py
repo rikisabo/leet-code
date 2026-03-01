7e3ffdcc22c4b1c7b7385f7b80b79b4cc2cc4615
Rivka Sabo <ns9760580@gmail.com>
2026-03-01 19:56:11 +0200

    Time: 4 ms (90.34%), Space: 12.4 MB (56.39%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,16 +4,7 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x < 0 or (x % 10 == 0 and x != 0):
+        if x < 0:
             return False
-
-        rev = 0
-        while x > rev:
-            rev = rev * 10 + (x % 10)
-            x //= 10
-
-        # לזוגי: x == rev
-        # לאי־זוגי: rev כולל ספרה אמצעית, אז משווים x == rev//10
-        return x == rev or x == rev // 10
-
-            
+        s = str(x)
+        return s == s[::-1]
