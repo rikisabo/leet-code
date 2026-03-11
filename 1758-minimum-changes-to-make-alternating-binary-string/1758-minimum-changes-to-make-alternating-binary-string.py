e3638dd1120a18c570563f506ab227c38c9c07e9
Rivka Sabo <ns9760580@gmail.com>
2026-03-11 12:11:16 +0200

    Time: 15 ms (74.43%), Space: 12.5 MB (83.14%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -1,23 +1,15 @@
 class Solution(object):
     def minOperations(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        mismatch1=0
-        mismatch2=0
-        for i,ch in enumerate(s):
-            ch1= '0' if i%2==0 else '1'
-            ch2= '1' if i%2==0 else '0'
-            if ch!=ch1:
-                mismatch1+=1
+        count1 = 0  # מול 010101...
+        count2 = 0  # מול 101010...
 
-            if ch!=ch2:
-                print(ch2,ch)
-                mismatch2+=1
-        return min(mismatch1,mismatch2)
+        for i, ch in enumerate(s):
+            expected1 = '0' if i % 2 == 0 else '1'
+            expected2 = '1' if i % 2 == 0 else '0'
 
+            if ch != expected1:
+                count1 += 1
+            if ch != expected2:
+                count2 += 1
 
-        
-
-        
+        return min(count1, count2)
